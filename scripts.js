0eb72d1baf757b66916408b243e06c0a8d46b789
Adrian SanPedro <adrian.r.sanpedro@gmail.com>
2020-10-02 16:56:38 -0700

    HTML id additions to handle JS add new book functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let myLibrary = [];
 
-myLibrary.push(new Book('Cat and Hat', 'Dr. Seuss', 11, false));
+let bookListElement = document.getElementById('book-list');
+
+myLibrary.push(new Book('Cat and Hat', 'Dr. Seuss', 11, true));
 myLibrary.push(
     new Book('Legend of Dick Dastardly', 'Cornelias Snake', 1060, false)
 );
@@ -25,19 +27,48 @@ function Book(title, author, pages, read) {
 }
 
 function getBooksInLibrary() {
-    let bookListElement = document.getElementById('book-list');
-
     for (let book of myLibrary) {
-        bookListElement.innerHTML += getBookListItemElement(book.title);
+        addBookListElement(book);
     }
 }
 
-function getBookListItemElement(title) {
-    return '<li>' + title + '</li>';
+function getBookListItemElement(book) {
+    return (
+        '<li id="book-list-item"><em>' +
+        book.title +
+        '</em><br>by: ' +
+        book.author +
+        '<br>Page Count: ' +
+        book.pages +
+        ' pages<br>' +
+        getIsReadContent(book.read) +
+        '</li>'
+    );
 }
 
-function addBookToLibrary() {}
+function getIsReadContent(isRead) {
+    return 'Read? <strong>' + (isRead ? 'Yes' : 'No') + '</strong>';
+}
 
-function showAddBackForm() {}
+function addBookListElement(book) {
+    bookListElement.innerHTML += getBookListItemElement(book);
+}
+
+function addNewBookToLibrary(event) {
+    event.preventDefault();
+
+    let newBook = new Book(
+        document.getElementById('title-input').value,
+        document.getElementById('author-input').value,
+        document.getElementById('number-of-pages-input').value,
+        document.querySelector('input[name=read]:checked').value
+    );
+
+    addBookListElement(newBook);
+}
 
 getBooksInLibrary();
+
+document
+    .getElementById('submit-button')
+    .addEventListener('click', addNewBookToLibrary);
